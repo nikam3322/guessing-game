7a40a3c097b6bbc2d9bde6f8327e29219d3dfc80
nikam3322 <nikam3322@gmail.com>
2023-09-09 15:57:19 +0200

    feat: solved

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.low = min;
+        this.high = max;
     }
 
     guess() {
-
+        return this.middle = Math.round((this.high + this.low) / 2);
     }
 
     lower() {
-
+        return this.high = this.middle;
     }
 
     greater() {
-
+        return this.low = this.middle;
     }
 }
 
